55d2ff98040160078bb21eddfab0ecc7b222601d
RWPicklechips <hminicucci@gmail.com>
2019-12-09 10:09:25 -0500

    Tory Bug Fix

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -22,8 +22,8 @@ const onFailure = message => {
 
 const onSignupSuccess = () => {
   onSuccess('welcome to the world of the Game')
-  $('.after-auth').show()
-  $('.before-auth').hide()
+  // $('.after-auth').show()
+  // $('.before-auth').hide()
 }
 
 const onSignupFailure = () => {
